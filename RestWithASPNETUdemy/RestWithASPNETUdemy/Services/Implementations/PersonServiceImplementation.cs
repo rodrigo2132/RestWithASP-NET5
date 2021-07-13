4fc67cc3c31e5731ec1c5dac35be7b00b04b506d
Rodrigo Dias <rodrigodias_bsb@outlook.com>
2021-07-13 12:35:51 -0300

    Atualização antes da conexão com o banco de dados

---
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,65 @@
+﻿using RestWithASPNETUdemy.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+         
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+
+            for (int i = 0; i < 8; i++)
+            {
+                Person p = MockePerson(i);
+                persons.Add(p);
+            }      
+
+            return persons;
+        }
+
+        private Person MockePerson(int i)
+        {
+            return new Person
+            {
+                Id = i,
+                FirstName = "Person Name" + i,
+                LastName = "Lima" + i,
+                Address = "Brasília - DF" + i,
+                Gender = "Male" + i
+            };
+        }
+
+        public Person FindByID(long id)
+        {
+            return new Person 
+            {
+                Id = 1,
+                FirstName = "Rodrigo",
+                LastName = "Lima",
+                Address = "Brasília - DF",
+                Gender = "Male"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+    }
+}
--- RestWithASPNETUdemy/RestWithASPNETUdemy/Startup.cs
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Startup.cs
@@ -11,6 +11,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using RestWithASPNETUdemy.Services;
+using RestWithASPNETUdemy.Services.Implementations;
 
 namespace RestWithASPNETUdemy
 {
@@ -32,6 +34,9 @@ namespace RestWithASPNETUdemy
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "RestWithASPNETUdemy", Version = "v1" });
             });
+
+            // Injeção de Dependencia
+            services.AddScoped<IPersonService, PersonServiceImplementation>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
